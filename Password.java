09cc768f10cfd81d35a6625aa17bc12db23b7198
Jonathan-Ma <54727124+Jonathan-Ma@users.noreply.github.com>
2021-01-27 18:11:53 -1000

    Update Password.java

--- Password.java
+++ Password.java
@@ -5,45 +5,33 @@ import java.util.regex.Pattern;
 public class Password{
     public static final int pw_Length = 8;
     public static void main(String[] args) {
-        do{
-        Scanner keyboard = new Scanner(System.in);
         String password;
-        
-        System.out.println("Please enter a password with least 8 characters and must contain at least two digits and one special symbol(!#$%&-_)");
-        System.out.print(">>> ");
-         password = keyboard.nextLine();
-         
-         if(password.length() >= pw_Length){
-            System.out.println("Must be at least 8 characters");
-         if(pwValidator(password)){
+        Scanner keyboard = new Scanner(System.in);
+        do{
+            System.out.println("Please enter a password with least 8 characters and must contain at least two digits and one special symbol(!#$%&-_)");
+            System.out.print(">>> ");
+            password = keyboard.nextLine();
+            if(password.length() <= pw_Length){
+                System.out.println("Must be at least 8 characters");
+            }
+            else if(pwValidator(password)){
                 System.out.println("Password is valid");
             }
-        }
-         //passes the input to matcher
-        if(pwValidator(password)){
-            System.out.println("Password is valid");
-        }
-         
-        else{
-            System.out.println("Password incorrect, try another one");
-            
-        }
-    } while(pwValidator(password) == false);
+            else{
+                System.out.println("Password incorrect, try another one");
+            }
+        } while(pwValidator(password) == false);
     }
     //a validator method 
     public static boolean pwValidator(String password) {
-        
-
         Pattern pattern;
         Matcher matcher;
         //requirements for password
-        String validPassword = "^((?=.*[0-9].*[0-9])(?=.*[a-zA-Z])(?=.*[!#$%&-_])).{8,20}).*$";
-        
-
+        String validPassword = "^((?=.*[0-9].*[0-9])(?=.*[a-zA-Z])(?=.*[!#$%&-_])).{8,20}.*$";
         pattern = Pattern.compile(validPassword);
         matcher = pattern.matcher(password);
         return matcher.matches();
     }
-        
 }
+ 
  
